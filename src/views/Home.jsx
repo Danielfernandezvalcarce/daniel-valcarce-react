116e998f77f956861afc4a2dc6b3b862ff73587a
Daniel Valcarce <dvalcarce93@gmail.com>
2023-06-15 23:27:07 +0200

    Timeline comenzado

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './styles/HomeStyle.css'
+import './../components/Timeline';
+import Timeline from './../components/Timeline';
 
 function Home() {
     return (
         <>
             <main>
-            <div className="text-white text-center">
-                <h1 id='nombreMain'>DANIEL VALCARCE</h1>
-                <div className='d-flex text-center justify-content-center'>
-                    <h3 id='Description'>FULL STACK</h3> <h3 class='text-warning'> SOFTWARE DEVELOPER</h3>
-                </div>
-            </div>
+                <section id="pesentacion">
+                    <div className="text-white text-center">
+                    <h1 id='nombreMain'>DANIEL VALCARCE</h1>
+                        <div className='d-flex text-center justify-content-center'>
+                            <h3 id='Description'>FULL STACK</h3> <h3 class='text-warning'> SOFTWARE DEVELOPER</h3>
+                        </div>
+                    </div>
+                </section>
+                <section id="">
+                    <Timeline />
+                </section>
             </main>
         </>
     );
