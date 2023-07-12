99308807b205167c4f2c01cf5945dddafdc76599
Daniel Valcarce <dvalcarce93@gmail.com>
2023-07-13 00:59:03 +0200

    13 de julio de 2023

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,11 +3,7 @@ import VisibilitySensor from "react-visibility-sensor";
 import "./styles/ResumeStyle.css";
 
 export function Resume() {
-  const [isVisible, setIsVisible] = useState(false);
 
-  const handleVisibilityChange = (isVisible) => {
-    setIsVisible(isVisible);
-  };
 
   /** FALTA PONER COLORES DE BORDE
    * FALTA PONER ICONOS
@@ -19,14 +15,12 @@ export function Resume() {
    */
 
   return (
-    <div class="text-white d-flex flex-wrap py-4">
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-light text-black bg-opacity-25 p-4 mb-4">
-          <h4>WELCOME TO MY PORTFOLIO</h4>
+    <div class="text-white d-flex flex-wrap py-4 col-10 mx-auto p-2">
+        <div class="py-5 my-4 text-center align-middle">
+          <h4 id="welcomePortfolio">WELCOME TO MY PORTFOLIO</h4>
         </div>
-      </VisibilitySensor>
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-secondary p-4 ml-auto mb-4">
+
+        <div class="mainDescription p-4 mb-4 custom-size-1">
           <p>
             Hello, I'm Daniel Valcarce, a dedicated and responsible Full Stack
             Developer with a passion for creating innovative web solutions. With
@@ -35,9 +29,8 @@ export function Resume() {
             valuable experience in various IT projects.
           </p>
         </div>
-      </VisibilitySensor>
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-info p-4 mb-4">
+
+        <div class="mainDescription p-4 mb-4 custom-size-2">
           <p>
             I hold a degree in Web Development, which has provided me with a
             solid foundation in the latest technologies and industry best
@@ -46,9 +39,8 @@ export function Resume() {
             field.
           </p>
         </div>
-      </VisibilitySensor>
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-warning p-4 ml-auto mb-4">
+
+        <div class="mainDescription p-4 mb-4 custom-size-3">
           <p>
             One of my recent achievements is establishing my own company called
             Web Tek Innovations. As the founder and CEO, I have led a team of
@@ -58,9 +50,8 @@ export function Resume() {
             collaboration, and client satisfaction.
           </p>
         </div>
-      </VisibilitySensor>
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-danger p-4 mb-4">
+
+        <div class="mainDescription p-4 mb-4 custom-size-4">
           <p>
             In addition to my professional work, I also dedicate my time as a
             volunteer web developer for "Palma Compasiva," a non-profit
@@ -68,9 +59,8 @@ export function Resume() {
             meaningful causes and make a positive impact in the community.
           </p>
         </div>
-      </VisibilitySensor>
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-success p-4 ml-auto mb-4">
+
+        <div class="mainDescription p-4 mb-4 custom-size-5">
           <p>
             With a keen eye for detail, I strive to create elegant and
             user-friendly web solutions that meet the unique needs of
@@ -79,16 +69,15 @@ export function Resume() {
             testament to my dedication and expertise.
           </p>
         </div>
-      </VisibilitySensor>
-      <VisibilitySensor onChange={handleVisibilityChange} partialVisibility>
-        <div class="rounded bg-primary p-4 mb-4">
+
+        <div class="mainDescription p-4 mb-4 custom-size-6">
           <p>
             Thank you for visiting my portfolio. Feel free to explore my
             projects and get in touch to discuss how I can help you achieve your
             web development goals. Let's create something exceptional together!
           </p>
         </div>
-      </VisibilitySensor>
+
     </div>
   );
-}
+};
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import './styles/HomeStyle.css';
-import { Resume } from '../components/Resume';
-
+import React from "react";
+import "./styles/HomeStyle.css";
+import { Resume } from "../components/Resume";
+import Flecha from "../assets/img/flecha.png";
 
 function Home() {
-    return (
-        <>
-            <main>
-                <section id="pesentacion">
-                    <div className="text-white text-center">
-                    <h1 id='nombreMain'>DANIEL VALCARCE</h1>
-                        <div className='d-flex text-center justify-content-center'>
-                            <h3 id='Description'>SOFTWARE</h3> <h3 class='text-warning'>FULL STACK WEB DEVELOPER.</h3>
-                        </div>
-                    </div>
-                </section>
-                <section id="">
-                    <Resume />
-                </section>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <section id="pesentacion">
+          <div className="text-white text-center">
+            <h1 id="nombreMain">DANIEL VALCARCE</h1>
+            <div className="d-flex text-center justify-content-center">
+              <h3 id="Description">SOFTWARE</h3>{" "}
+              <h3 class="text-warning">FULL STACK WEB DEVELOPER.</h3>
+            </div>
+            <div>
+                <img src={Flecha} alt="" width="200px" class="animated-arrow"/>
+            </div>
+          </div>
+        </section>
+        <section id="">
+          <Resume />
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Home;
