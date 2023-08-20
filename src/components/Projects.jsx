2f8328b593bdce7a7d495ddafc3f86be55c34898
Daniel Valcarce <dvalcarce93@gmail.com>
2023-08-20 21:05:12 +0200

    ADD contact form + WTI

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,46 @@
-import "./styles/ProjectsStyle.css"
+import "./styles/ProjectsStyle.css";
 
 export function Projects() {
   return (
     <>
       <section className="projects">
         <div className="project">
+          <h4 className="projectTitle">Web Tek Innovations</h4>
+          <a href="https://webtekinnovations.com/" className="enlaceProyecto">
+            WebTekInnovations.com
+          </a>
+          <div className="technologies">
+            <h5>Technologies:</h5>
+            <p>
+              HTML, Bootstrap, CSS, SASS, JavaScript, React, React-Bootstrap
+            </p>
+          </div>
+          <p>
+            As the CEO and Lead Programmer of Web Tek Innovations, I have always
+            strived to position our company at the forefront of web technology
+            and innovation. Established in 2023, my journey began with an
+            unyielding passion: to redefine the digital domain and provide
+            software services that stand unmatched in quality and functionality.
+            Under my leadership, Web Tek Innovations is not just a company—it's
+            a vision. A vision to bridge technological disparities and to ensure
+            businesses receive only the finest web solutions. My expertise in
+            PHP and JavaScript, coupled with an adept mastery of frameworks like
+            Laravel and React, has been pivotal in steering our projects to
+            success. This portfolio serves as a testament to our technical
+            achievements. Each showcased project underscores our dedication to
+            excellence, our innovative spirit, and my personal commitment to
+            spearheading web software development. Let this portfolio offer a
+            glimpse into our journey as we persistently push the boundaries of
+            web development, and as I continue to lead Web Tek Innovations with
+            unwavering passion and precision. Again, feel free to modify any
+            sections to better fit your voice and perspective.
+          </p>
+        </div>
+        <div className="project">
           <h4 className="projectTitle">Ascenso a Cabo</h4>
-          <a href="https://ascensoacabo.com/" className="enlaceProyecto">ascensoacabo.com</a>
+          <a href="https://ascensoacabo.com/" className="enlaceProyecto">
+            ascensoacabo.com
+          </a>
           <div className="technologies">
             <h5>Technologies:</h5>
             <p>
@@ -28,7 +62,9 @@ export function Projects() {
 
         <div className="project">
           <h4 className="projectTitle">Tu Permanencia (Working on it)</h4>
-          <a href="https://danielvalcarce.com/" className="enlaceProyecto">tupermanencia.com</a>
+          <a href="https://danielvalcarce.com/" className="enlaceProyecto">
+            tupermanencia.com
+          </a>
           <div className="technologies">
             <h5>Technologies:</h5>
             <p>
@@ -51,7 +87,9 @@ export function Projects() {
 
         <div className="project">
           <h4 className="projectTitle">Palma Compasiva</h4>
-          <a href="https://palmacompasiva.org/" className="enlaceProyecto">palmacompasiva.org</a>
+          <a href="https://palmacompasiva.org/" className="enlaceProyecto">
+            palmacompasiva.org
+          </a>
           <div className="technologies">
             <h5>Technologies:</h5>
             <p>WordPress</p>
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,4 +1,5 @@
 import "./styles/ContactStyle.css";
+import { ContactForm } from "../components/ContactUsForm";
 
 export function Contact() {
   return (
@@ -7,25 +8,12 @@ export function Contact() {
         <section id="contact">
           <h2>Contact</h2>
           <p>
-            If you would like to get in touch, please feel free to send me an
-            email or connect with me on LinkedIn.
+            If you would like to get in touch, please feel free to send me a message or contact me on LinkedIn.
           </p>
 
           <div className="contact-details">
             <div className="elementoContacto">
-              <h3>Email</h3>
-              <p>
-                <a href="mailto:dvalcarce93@gmail.com">dvalcarce93@gmail.com</a>
-              </p>
-            </div>
-
-            <div className="elementoContacto">
-              <h3>LinkedIn</h3>
-              <p>
-                <a href="https://www.linkedin.com/in/daniel-f-valcarce/">
-                  Daniel Valcarce
-                </a>
-              </p>
+              <ContactForm></ContactForm>
             </div>
           </div>
         </section>
